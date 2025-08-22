5961335ca423522cfa50f6a0feff4f8a3d4a557b
dvargas2396 <david.vargas@ibancar.com>
2025-08-22 18:05:10 +0200

    modificaciones para generar version .parquet a LTSC

--- 4.Areas/1.Finance/Reporting/Loan_Tape_Stratification_Collections/reporteria_finanzas.py
+++ 4.Areas/1.Finance/Reporting/Loan_Tape_Stratification_Collections/reporteria_finanzas.py
@@ -2110,28 +2110,33 @@ def loan_tape_stratification_collection(loan_tape_copy, final, year, destiny, ta
     period_index.loc[idx_amortiza, 'AC Interest'] = period_index.loc[idx_amortiza, 'AC Interest'] + period_index.loc[idx_amortiza, 'AC Origination Fee VAT']
     period_index.loc[idx_amortiza, 'AC Origination Fee VAT'] = 0
     
-    ###########################################################################
-    if destiny == 'xlsx':         
-        print('Generando el excel...')         
-        period_index.to_excel(             
-            f'output/7. Loan Tape Stratification Collections {datetime.date.today()}_{geo}.xlsx',             
-            index=False         
-        )      
-    elif destiny == 'parquet':         
-        print('Generando el parquet...')         
-        period_index.to_parquet(             
-            f'output/7. Loan Tape Stratification Collections {datetime.date.today()}_{geo}.parquet',             
-            index=False,             
-            engine="pyarrow"  # más rápido y recomendado         
-        )      
-    elif destiny == 'db':         
-        print('Cargando en la base de datos...')         
-        database_engine(geo, period_index, table_name, if_exists)      
-    else:         
-        print('El dataframe sólo está en la memoria, no se ha guardado en ningún archivo externo.')         
+
+    # ============================================================================================
+    if destiny == 'xlsx':
+        print('Generando el excel...')
+        period_index.to_excel(
+            f'7. Loan Tape Stratification Collections {datetime.date.today()}_{geo}.xlsx',
+            index=False
+        )
+
+    # VERSION .PARQUET AÑADIDA POR DAVID
+    elif destiny == 'parquet':
+        print('Generando el parquet...')
+        period_index.to_parquet(
+            f'7. Loan Tape Stratification Collections {datetime.date.today()}_{geo}.parquet',
+            index=False,
+            engine="pyarrow"
+        )
+
+    elif destiny == 'db':
+        print('Cargando en la base de datos...')
+        database_engine(geo, period_index, table_name, if_exists)
+
+    else:
+        print('El dataframe sólo está en la memoria, no se ha guardado en ningún archivo externo.')
         print('Revise el parámetro destiny de la función')
 
-    
+     # ============================================================================================
     
     #Agrupamos por year month la recuperación de cada uno de los buckets que tenemos
     cols = ['AC Payment Amount', 'AC Principal', 'AC Origination Fee', 'AC Origination Fee VAT', 'AC Interest', 'AC Interest VAT',
